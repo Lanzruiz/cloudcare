8660d93ee61f24573f67476a97c053068c6dc73a
Lanz Ruiz <Lanzruizdesigns@gmail.com>
2022-08-18 18:39:52 +0800

    adding get profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -91,11 +91,41 @@ router.post('/', [ auth, [
       await profile.save();
       res.json(profile);
    } catch(err) {
-     console.error(err.message)
-     res.status(500).send('Server Error')
+      console.error(err.message)
+      res.status(500).send('Server Error')
    }
 
 
 });
 
+
+// @route    Get api/profile
+// @desc     Get all profile profile
+// @access   Public
+router.get('/', async (req, res) => {
+   try {
+      const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+      res.json(profiles);
+   } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+   }
+});
+
+// @route    Get api/profile/user/:user_id
+// @desc     Get profile by user Id
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+   try {
+      const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar'])
+      if(!profile) res.status(400).json({ msg: 'There is no profile for this user' })
+      res.json(profile);
+   } catch (err) {
+      if(err.kind == 'ObjectId') {
+         return res.status(400).json({ msg: 'Profile not found' })
+      }
+      console.error(err.message)
+      res.status(500).send('Server Error')
+   }
+});
 module.exports = router;
