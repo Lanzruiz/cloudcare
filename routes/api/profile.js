be42bff702eaed8439469d52e9e1940a428e0d14
Lanz Ruiz <Lanzruizdesigns@gmail.com>
2022-08-18 20:21:02 +0800

    implement profile with use delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -128,4 +128,23 @@ router.get('/user/:user_id', async (req, res) => {
       res.status(500).send('Server Error')
    }
 });
+
+
+// @route    Delete api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+
+router.delete('/', auth, async (req, res) => {
+   try {
+      
+      await Profile.findOneAndRemove({ user: req.user.id });
+      await User.findOneAndRemove({ _id: req.user.id})
+      res.json({ msg: 'User deleted'});
+   } catch (err) {
+      
+      console.error(err.message)
+      res.status(500).send('Server Error')
+   }
+});
+
 module.exports = router;
